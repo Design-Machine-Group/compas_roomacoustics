c4f86177199072d189aa69a439b66b081bbddccb
Tomas Mendez Echenagucia <tmendeze@uw.edu>
2020-08-17 15:19:17 -0700

    testing toms libigl stuff

--- examples/libigl_reflections.py
+++ examples/libigl_reflections.py
@@ -2,12 +2,14 @@ import math
 import numpy as np
 import compas_libigl as igl
 
-from compas.geometry import add_vectors, scale_vector
+from compas.geometry import add_vectors, scale_vector, normalize_vector
 from compas.geometry import Point, Vector, Line, Plane, Sphere
 from compas.geometry import Reflection, Translation
 from compas.datastructures import Mesh
 
 from compas_viewers.objectviewer import ObjectViewer
+
+for i in range(50): print()
 
 # ==============================================================================
 # Input geometry
@@ -19,8 +21,8 @@ mesh.quads_to_triangles()
 z = mesh.vertices_attribute('z')
 zmin = min(z)
 
-T = Translation.from_vector([0, 0, -zmin])
-mesh.transform(T)
+# T = Translation.from_vector([0, 0, -zmin])
+# mesh.transform(T)
 
 # ==============================================================================
 # Rays
@@ -55,13 +57,15 @@ for x, y, z in hemi:
 # Intersections & Reflections
 # ==============================================================================
 
-intersections = []
-# reflections = []
+
 
 ground = Plane([0, 0, 0], [0, 0, 1])
 
 faces = list(mesh.faces())
-
+intersections = []
+reflections = []
+normals = []
+centers = []
 for ray in rays:
     base, vector = ray
     hits = igl.intersection_ray_mesh(ray, mesh)
@@ -72,9 +76,15 @@ for ray in rays:
             n = scale_vector(mesh.face_normal(i), -1)
             x = add_vectors(base, scale_vector(vector, d))
             p = Point(*x)
-            # r = base.transformed(Reflection.from_plane((p, n)))
+            r = base.transformed(Reflection.from_plane((p, n)))
+            n = add_vectors(n, x)
+            n = Point(*n)
             intersections.append(p)
-            # reflections.append(r)
+            reflections.append(r)
+            normals.append(n)
+            centers.append(mesh.face_centroid(i))
+
+
 
 # ==============================================================================
 # Visualisation
@@ -84,9 +94,33 @@ viewer = ObjectViewer()
 
 viewer.add(mesh, settings={'color': '#cccccc', 'opacity': 0.7, 'vertices.on': False})
 
-for p in intersections:
+for i in range(len(intersections) - 132):
+    p = intersections[i]
+    r = reflections[i]
+    n = normals[i]
+    c = centers[i]
     viewer.add(Line(base, p), settings={'edges.color': '#ff0000', 'edges.width': 2})
-    # viewer.add(Line(p, r), settings={'edges.color': '#0000ff', 'edges.width': 2})
+    viewer.add(Line(p, r), settings={'edges.color': '#0000ff', 'edges.width': 2})
+    viewer.add(Line(p, n), settings={'edges.width': 2})
+
+    viewer.add(Point(*c), settings={'vertices.size': 10,
+                                    'vertices.on': True,
+                                    'vertices.color':'#0000ff'})
+
+    # viewer.add(Point(*r), settings={'vertices.size': 10,
+    #                                 'vertices.on': True,
+    #                                 'vertices.color':'#ff0000'})
+
+# for fk in mesh.faces():
+#     c = mesh.face_centroid(fk)
+#     n = normalize_vector(mesh.face_normal(fk))
+#     pt = add_vectors(c, n)
+#     viewer.add(Line(c, pt), settings={'edges.width': 2})
+#     # viewer.add(Point(*pt), settings={'vertices.size': 10,
+#     #                             'vertices.on': True,
+#     #                             'vertices.color':'#ff0000'})
+
+
 
 viewer.update()
 viewer.show()
